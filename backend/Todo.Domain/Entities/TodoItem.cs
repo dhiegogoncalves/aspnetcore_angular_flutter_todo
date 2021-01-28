d39a718c2f8ca134db230e7ebc6b7047934864ae
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2021-01-27 22:08:29 -0400

    Implementação dos Commands.

--- backend/Todo.Domain/Entities/TodoItem.cs
+++ backend/Todo.Domain/Entities/TodoItem.cs
@@ -4,18 +4,18 @@ namespace Todo.Domain.Entities
 {
     public class TodoItem : Entity
     {
-        public Todo(string title, DateTime date, string user)
+        public TodoItem(string title, string user, DateTime date)
         {
             Title = title;
             Done = false;
-            Date = date;
             User = user;
+            Date = date;
         }
 
         public string Title { get; private set; }
         public bool Done { get; private set; }
-        public DateTime Date { get; private set; }
         public string User { get; private set; }
+        public DateTime Date { get; private set; }
 
         public void MarkAsDone()
         {
