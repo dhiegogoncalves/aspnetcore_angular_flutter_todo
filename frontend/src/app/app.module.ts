672881790b3939582510152981ab3c14d2ae2ead
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2021-01-29 10:46:40 -0400

    Implementação dos componentes card e button.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
